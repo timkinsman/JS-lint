d0f3bd658a9c152925d3cede3c074e25e45da6d2
timkinsman <tkinsm@gmail.com>
2020-08-29 12:53:37 +0930

    eslintrc changes

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const { ESLint } = require("eslint");
 
 const run = async () => {
-  // 1. Create an instance.
   const eslint = new ESLint();
-
-  // 2. Lint files.
   const results = await eslint.lintFiles("**/*.js");
-
-  // 3. Format the results.
   const formatter = await eslint.loadFormatter("stylish");
   const resultText = formatter.format(results);
-
-  // 4. Output it.
   console.log(resultText);
 }
 
