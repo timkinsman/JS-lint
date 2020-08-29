9571b65c2bb8d26a091aa5e2268aa30149920bee
timkinsman <tkinsm@gmail.com>
2020-08-29 12:29:25 +0930

    eslintrc changes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const glob = require('@actions/glob');
 const { ESLint } = require("eslint");
 
-(async function main() {
+const run = async () => {
   // 1. Create an instance.
   const eslint = new ESLint();
 
   // 2. Lint files.
-  const results = await eslint.lintFiles(["**/*.js"]);
+  const results = await eslint.lintFiles("**/*.js");
 
   // 3. Format the results.
   const formatter = await eslint.loadFormatter("stylish");
@@ -16,17 +13,9 @@ const { ESLint } = require("eslint");
 
   // 4. Output it.
   console.log(resultText);
-})().catch((error) => {
+}
+
+run().catch(error => {
   process.exitCode = 1;
   console.error(error);
 });
-
-
-const run = async () => {
-  const globber = await glob.create('**/*.js');
-  for await (const file of globber.globGenerator()) {
-    console.log(file);
-  }
-}
-
-//run();
