2360d9a727a7ca80009711a1fecaf9e20fc86957
timkinsman <tkinsm@gmail.com>
2020-08-29 12:16:48 +0930

    index.js changes

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const glob = require('@actions/glob');
+const { ESLint } = require("eslint");
+
+(async function main() {
+  // 1. Create an instance.
+  const eslint = new ESLint();
+
+  // 2. Lint files.
+  const results = await eslint.lintFiles(["**/*.js"]);
+
+  // 3. Format the results.
+  const formatter = await eslint.loadFormatter("stylish");
+  const resultText = formatter.format(results);
+
+  // 4. Output it.
+  console.log(resultText);
+})().catch((error) => {
+  process.exitCode = 1;
+  console.error(error);
+});
+
 
 const run = async () => {
   const globber = await glob.create('**/*.js');
@@ -9,4 +29,4 @@ const run = async () => {
   }
 }
 
-run();
+//run();
