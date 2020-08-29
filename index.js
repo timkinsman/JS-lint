67b27a15fddb740cab97e6efff0c22791bfd9529
timkinsman <tkinsm@gmail.com>
2020-08-29 15:37:26 +0930

    autofixes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const core = require('@actions/core');
 const { ESLint } = require("eslint");
 
 const run = async () => {
+  // 1. Create an instance with the `fix` option.
   const eslint = new ESLint();
+
+  // 2. Lint files. This doesn't modify target files.
   const results = await eslint.lintFiles("**/*.js");
+  
+  // 3. Modify the files with the fixed code.
+  if(core.getInput('autofixes')) {
+    await ESLint.outputFixes(results);
+  }
+
+  // 4. Format the results.
   const formatter = await eslint.loadFormatter("stylish");
   const resultText = formatter.format(results);
+
+  // 5. Output it.
   console.log(resultText);
 }
 
