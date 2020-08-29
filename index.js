1aee8e31dacc13a26646768d41a395e0c9623ab1
timkinsman <tkinsm@gmail.com>
2020-08-29 11:38:13 +0930

    index.js changes

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const glob = require('@actions/glob');
 
+console.log('Test1')
+
 (async function() {
   const globber = await glob.create('**/*.js')
+  console.log('Test2')
 
   for await (const file of globber.globGenerator()) {
     console.log(file)
+    console.log('Test3')
   }
 })()
+
+console.log('Test4')
